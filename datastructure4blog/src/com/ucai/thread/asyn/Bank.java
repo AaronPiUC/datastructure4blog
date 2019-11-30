426a6dc6237ab16ff838db02c801b42f0a731b12
ucai <AaronPi@163.com>
2019-11-30 11:11:12 +0800

    添加线程的创建

--- datastructure4blog/src/com/ucai/thread/asyn/Bank.java
+++ datastructure4blog/src/com/ucai/thread/asyn/Bank.java
@@ -1,27 +1,50 @@
 package com.ucai.thread.asyn;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank implements Runnable{
     private int tickets = 100;
-    private ReentrantLock myLock = new ReentrantLock(true);
-    @Override
+    private ReentrantLock myLock = new ReentrantLock();
+    /*@Override
     public void run() {
-
-        while(tickets > 0){
-            try{
-                myLock.lock();
-                    if(tickets > 0){
-                        System.out.println(Thread.currentThread().getName()+"售出第"+tickets);
-                        tickets = tickets -1;
-                }
-            }finally {
-                myLock.unlock();
+        myLock.lock();
+        try{
+            Condition con = myLock.newCondition();
+            System.out.println(con);
+            while(tickets > 0){
+                System.out.println(Thread.currentThread().getName()+"售出第"+tickets);
+                tickets = tickets -1;
             }
+        }finally {
+            myLock.unlock();
+        }
+        System.out.println(Thread.currentThread().getName()+"窗口票已售完");
+    }*/
+
+    /*@Override
+    public synchronized void run() {
+        while(tickets > 0){
+            System.out.println(Thread.currentThread().getName()+"售出第"+tickets);
+            tickets = tickets -1;
         }
 
         System.out.println(Thread.currentThread().getName()+"窗口票已售完");
+    }*/
+
+    @Override
+    public void run() {
+        synchronized (this){
+            while(tickets > 0){
+                System.out.println(Thread.currentThread().getName()+"售出第"+tickets);
+                tickets = tickets -1;
+            }
+
+            System.out.println(Thread.currentThread().getName()+"窗口票已售完");
+        }
+
     }
+
     public static void main(String []args) {
         Bank b = new Bank();
         for (int i = 0; i < 5; i++) {
